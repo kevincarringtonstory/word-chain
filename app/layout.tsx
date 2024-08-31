dbac44eec4f5ed2da7708a255104129ff97da43c
Kevin Carrington <Kevington99@gmail.com>
2024-08-30 21:54:00 -0700

    update layout for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Word Chain Game',
   description: 'Play the Word Chain Game',
+  viewport:
+    'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0',
 };
 
 export default function RootLayout({
